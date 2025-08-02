c56506aa020594c0051d67a8850f198eabf6b4cc
dreadPaiertDavincci <dreadpaiertdavinci@gmail.com>
2025-08-03 02:46:34 +0300

    sad

--- src/Page/P404.jsx
+++ src/Page/P404.jsx
@@ -6,6 +6,9 @@ function P404(){
                <div className="ErrorMessageContinar">
                  <h1 id="M404">404</h1>
                </div>
+               <div className="HiddenButton404">
+                 HOME // 404
+               </div>
                <div className="P404Messages"> 
                 <div className="P404Titles">
                     <h1 id="P404Title">This is Awkward. <br />We Can't Find That Page.</h1>
