c9897c00658ceb94db1f83375ca93ff4fd99e824
dreadPaiertDavincci <dreadpaiertdavinci@gmail.com>
2025-08-09 14:45:57 +0300

    asd

--- src/Page/Navbar.jsx
+++ src/Page/Navbar.jsx
@@ -2,26 +2,24 @@ import "../Style/Nav.css";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaUserPlus } from "react-icons/fa6";
 import ShowLogInPage from "../../Componant/ShowLogInPage";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 import CartPage from "./CartPage";
+
 function NavBar({ cartItems, setCartItems }) { 
     const [showCartBar , setShowCartBar] = useState(false); 
-
-
     const [showLogin, setShowLogin] = useState(false);
-
 
     const cartToggle = () => { 
         setShowCartBar(!showCartBar);
-    }
+    }    
 
-
-
-    
     const ToggleShowPage = () => { 
         setShowLogin(!showLogin);
     }
+
+    const location = useLocation();
+
     const navItem = [
         {name: "Home", path : "/"},
         {name: "About", path:"/about"},
@@ -34,26 +32,44 @@ function NavBar({ cartItems, setCartItems }) {
             {name: "TermsPrivacy" , path:"/termsprivacy"}
         ]},
     ];
+
     const LoopNav = navItem.map((e, navIndex) => { 
+        const isActive = location.pathname === e.path;
+
         return(
            <li key={navIndex} className={`nav-item ${e.subPage ? 'has-dropdown' : ''}`}>
-             <Link to={e.path} id="LinkRouter" className="nav-link">
+             <Link 
+               to={e.path} 
+               id="LinkRouter" 
+               className={`nav-link ${isActive ? 'active' : ''}`}
+             >
                 {e.name}
                 {e.subPage && <span className="dropdown-arrow">▼</span>}
              </Link>
+
              {e.subPage && (
                 <ul className="dropdown">
-                    {e.subPage.map((sub, subIndex)=> (
-                        <li key={subIndex} id="subId">
-                            <Link to={sub.path} className="subLink">{sub.name}</Link>
-                        </li>
-                    ))}
+                    {e.subPage.map((sub, subIndex)=> {
+                        const isSubActive = location.pathname === sub.path;
+                        return (
+                            <li key={subIndex} id="subId">
+                                <Link 
+                                  to={sub.path} 
+                                  className={`subLink ${isSubActive ? 'active' : ''}`}
+                                >
+                                  {sub.name}
+                                </Link>
+                            </li>
+                        );
+                    })}
                 </ul>
              )} 
           </li>
         );
     });
+
     const totalCount = cartItems.reduce((acc, item) => acc + item.count, 0);
+
     return(
     <>
         <nav className="main-nav">
@@ -70,17 +86,9 @@ function NavBar({ cartItems, setCartItems }) {
                  <FaUserPlus />
                </div>
             </div>
-            
-
         </nav>
 
        {showLogin && <ShowLogInPage onClose={ToggleShowPage} />} 
-
-
-
-
-
-
        {showCartBar && <CartPage cartItems={cartItems} setCartItems={setCartItems} onClose={cartToggle} />} 
      </>
     );
